ed6217915a3474f0260fa1de917bc1b5b452b51f
Eric-Gona <eric.gona254@gmail.com>
2022-01-30 17:20:43 +0300

    adds configuration

--- config.py
+++ config.py
@@ -1,4 +1,3 @@
-"""
 import os
 
 class Config:
@@ -6,7 +5,7 @@ class Config:
     NEWS_API_SOURCE_URL = 'https://newsapi.org/v2/sources?language=en&apiKey={}'
 
     ARTICLES_API_BASE_URL = 'https://newsapi.org/v2/everything?q={}&apiKey={}'
-    NEWS_API_KEY = 2916d5e6b1384e98b9e5cf5822356016('NEWS_API_KEY')
+    NEWS_API_KEY = '2916d5e6b1384e98b9e5cf5822356016'('NEWS_API_KEY')
     
 
 class ProdConfig(Config):
@@ -19,4 +18,3 @@ config_options = {
     'development':DevConfig,
     'production':ProdConfig
 }
-"""
