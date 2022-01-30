d3ad62f49bb3a78e4ceed53e4573161bc4564ab9
Eric-Gona <eric.gona254@gmail.com>
2022-01-30 17:02:45 +0300

    adds imports in request.py

--- app/requests.py
+++ app/requests.py
@@ -1,4 +1,4 @@
-"""
+
 import urllib.request,json
 from .models import Source,Article
 import requests
@@ -9,7 +9,7 @@ source_url = None
 article_url = None
 base_url =None
 base_article_url = None
-
+"""
 def configure_request(app):
     global apiKey,source_url, base_article_url
     apiKey = app.config['NEWS_API_KEY']
