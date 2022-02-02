86cb2d440b986f2cbfce02a9fddc87064c2e2cb6
Eric-Gona <eric.gona254@gmail.com>
2022-02-02 13:24:59 +0300

    add gitignore

--- config.py
+++ config.py
@@ -1,9 +1,8 @@
 import os
 
 class Config:
-
+    SECRET_KEY= os.environ.get('SECRET_KEY')
     NEWS_API_SOURCE_URL = 'https://newsapi.org/v2/sources?language=en&apiKey={}'
-
     ARTICLES_API_BASE_URL = 'https://newsapi.org/v2/everything?q={}&apiKey={}'
     NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
     
--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
-export NEWS_API_KEY=2916d5e6b1384e98b9e5cf5822356016
-export SECRET_KEY=cheaky
+export NEWS_API_KEY='2916d5e6b1384e98b9e5cf5822356016'
+export SECRET_KEY='this is secret'
 
 python3 manage.py server
